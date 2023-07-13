dfcaa78558a52932820927aacf73c43b5a0607f7
unknown <dinabeysinghe@gmail.com>
2023-07-13 11:48:20 +0530

    create left and home content componets

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import LeftMenu from './LeftMenu'
+import HomeContent from './HomeContent'
 
 export default function Home() {
   return (
   <div>
       <h2>Home</h2>
-      <h2>Left Menu</h2>
-      <h2>Home Content</h2>
+      {/* <h2>Left Menu</h2>
+      <h2>Home Content</h2> */}
+      <LeftMenu/>
+      <HomeContent/>
   </div>
   )
 }
